6c69aee05903b7c74637b1baecafd449955da900
Mahin-Hasan <mahinmtrs@gmail.com>
2023-11-14 01:26:16 +0600

    added sign in & sign out functionality

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,23 +1,58 @@
-import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import { GoogleAuthProvider, getAuth, signInWithPopup, signOut } from "firebase/auth";
 import app from "../../firebase/firebase.init";
+import { useState } from "react";
 const Login = () => {
+    const [user, setUser] = useState([]);
     const auth = getAuth(app);
     const provider = new GoogleAuthProvider();
-    const handleGoogleSingIn = () =>{
-        signInWithPopup(auth,provider)
-        .then(result=>{
-            const user = result.user;
-            console.log(user);
-        })
-        .catch(error=>{
-            console.log('Error', error.message);
-        })
+    const handleGoogleSingIn = () => {
+        signInWithPopup(auth, provider)
+            .then(result => {
+                const loggedInUser = result.user;
+                console.log(loggedInUser);
+                setUser(loggedInUser)
+            })
+            .catch(error => {
+                console.log('Error', error.message);
+            })
+    }
+    const handleSignOut = () => {
+        signOut(auth)
+            .then(result => {
+                console.log(result);
+                setUser(null)
+            })
+            .catch(error => {
+                console.log(error);
+            })
+
     }
 
 
     return (
         <div>
-            <button onClick={handleGoogleSingIn}>Google Log in</button>
+            {/* div make btn conditional user ? logout : sing in */}
+
+            {
+                user ?
+                    <button onClick={handleSignOut}>Sign out</button>
+                    :
+                    <button onClick={handleGoogleSingIn}>Google Log in</button>
+            }
+            {/* <div>
+                <h3>User: {user?.displayName}</h3>
+            </div> */}
+            {/* another system using conditional rendering */}
+            {
+                user && <div>
+                    <h3>User: {user?.displayName}</h3>
+                    <p>Email: {user.email}</p>
+                    <img src={user.photoURL} alt="" />
+                </div>
+            }
+            {/* <div>
+                <h3>User: {user?.displayName}</h3>
+            </div> */}
         </div>
     );
 };
